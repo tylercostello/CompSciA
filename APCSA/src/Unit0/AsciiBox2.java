a07c9884dcca4de5b8edda6d2da9c81b9095df6d
tyler costello <34726618+tylercostello@users.noreply.github.com>
2020-01-31 09:59:26 -0800

    a

--- APCSA/src/Unit0/AsciiBox2.java
+++ APCSA/src/Unit0/AsciiBox2.java
@@ -10,18 +10,18 @@ public class AsciiBox2
 {
 	public static void main(String[] args)
 	{
-		System.out.println("+++++++++++++++++++++++++ " );
-		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA " );
+		System.out.println("++++++++++++++++++++++++++ " );
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAA " );
 		for (int i = 0; i<10; i++) {
 			if (i==5) {
-			System.out.println("++     Comp Sci A       ++" );
+				System.out.println("++     Comp Sci A       ++" );
 			}
 			else {
-			System.out.println("++                      ++" );
+				System.out.println("++                      ++" );
 			}
 		}
-		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA " );
-		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAA " );
+		System.out.println("++++++++++++++++++++++++++ " );
 
 		
 	}
