a4f12a33966bdcb43cd1696e4e3d273fe129db99
tyler costello <34726618+tylercostello@users.noreply.github.com>
2020-01-31 09:58:08 -0800

    Create AsciiBox2.java

---
+++ APCSA/src/Unit0/AsciiBox2.java
@@ -0,0 +1,28 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+package Unit0;
+public class AsciiBox2
+{
+	public static void main(String[] args)
+	{
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA " );
+		for (int i = 0; i<10; i++) {
+			if (i==5) {
+			System.out.println("++     Comp Sci A       ++" );
+			}
+			else {
+			System.out.println("++                      ++" );
+			}
+		}
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA " );
+		System.out.println("+++++++++++++++++++++++++ " );
+
+		
+	}
+}
