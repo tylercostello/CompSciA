809207221d7cbbc07401cde5b3ba53fc12497510
tyler costello <34726618+tylercostello@users.noreply.github.com>
2020-02-10 09:57:35 -0800

    units 4-6

---
+++ APCSA/src/Unit5/AvgLen.java
@@ -0,0 +1,12 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+//Date -
+package Unit5;
+public class AvgLen
+{
+   public static double go( String a, String b )
+	{
+		return 0;
+	}
+}
---
+++ APCSA/src/Unit5/FirstLastVowel.java
@@ -0,0 +1,13 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+//Date -
+package Unit5;
+
+public class FirstLastVowel
+{
+   public static String go( String a )
+	{
+		return "add code here";
+	}
+}
