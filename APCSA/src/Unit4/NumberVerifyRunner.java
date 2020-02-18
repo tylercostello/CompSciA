9709720a713f73b363678330be7e097ccb5f23d6
tyco333 <34726618+tyco333@users.noreply.github.com>
2020-02-18 10:04:21 -0800

    unit 4 test cases

--- APCSA/src/Unit4/NumberVerifyRunner.java
+++ APCSA/src/Unit4/NumberVerifyRunner.java
@@ -7,6 +7,7 @@
 //Lab  -
 package Unit4;
 import static java.lang.System.*;
+
 import java.util.Scanner;
 
 public class NumberVerifyRunner
@@ -14,10 +15,25 @@ public class NumberVerifyRunner
 	public static void main ( String[] args )
 	{
 		//add in input
-		System.out.println("5 is odd :: " + NumberVerify.isOdd(5));
-		System.out.println("5 is even :: " + NumberVerify.isEven(5));
+		Scanner keyboard = new Scanner(System.in);
+		System.out.print("Enter a whole number :: ");
+		int input = keyboard.nextInt();
+		System.out.println(input+ " is odd :: " + NumberVerify.isOdd(input));
+		System.out.println(input+ " is even :: " + NumberVerify.isEven(input));
+		
+		System.out.println("111 is odd :: " + NumberVerify.isOdd(111));
+		System.out.println("111 is even :: " + NumberVerify.isEven(111));
+		
+		System.out.println("2000 is odd :: " + NumberVerify.isOdd(2000));
+		System.out.println("2000 is even :: " + NumberVerify.isEven(2000));
+		//add in more test cases
+		System.out.println("-99 is odd :: " + NumberVerify.isOdd(-99));
+		System.out.println("-99 is even :: " + NumberVerify.isEven(-99));
+		
+		System.out.println("1111 is odd :: " + NumberVerify.isOdd(1111));
+		System.out.println("1111 is even :: " + NumberVerify.isEven(1111));
+		
 		System.out.println("-850 is odd :: " + NumberVerify.isOdd(-850));
 		System.out.println("-850 is even :: " + NumberVerify.isEven(-850));
-		//add in more test cases
 	}
 }
