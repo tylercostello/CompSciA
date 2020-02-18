f753e9473102e5521880029dcaf37d72bce9b60f
tyco333 <34726618+tyco333@users.noreply.github.com>
2020-02-18 09:57:10 -0800

    more test cases
    a

--- APCSA/src/Unit4/DiscountRunner.java
+++ APCSA/src/Unit4/DiscountRunner.java
@@ -19,7 +19,35 @@ public class DiscountRunner
 		double amt = keyboard.nextDouble();
 		amt=Discount.getDiscountedBill(amt);
 		System.out.print("Bill after discount :: ");
-		System.out.printf("%.2f\n",amt);    
+		System.out.printf("%.2f\n",amt);
+		
+		amt=Discount.getDiscountedBill(500);
+		System.out.print("Bill after discount :: ");
+		System.out.printf("%.2f\n",amt);   
+		
+		amt=Discount.getDiscountedBill(2500);
+		System.out.print("Bill after discount :: ");
+		System.out.printf("%.2f\n",amt);   
+		
+		amt=Discount.getDiscountedBill(4000);
+		System.out.print("Bill after discount :: ");
+		System.out.printf("%.2f\n",amt);   
+		
+		amt=Discount.getDiscountedBill(333.33);
+		System.out.print("Bill after discount :: ");
+		System.out.printf("%.2f\n",amt);  
+		
+		amt=Discount.getDiscountedBill(95874.2154);
+		System.out.print("Bill after discount :: ");
+		System.out.printf("%.2f\n",amt);  
 
 	}
 }
+/*Enter the original bill amount :: 123
+Bill after discount :: 123.00
+Bill after discount :: 500.00
+Bill after discount :: 2125.00
+Bill after discount :: 3400.00
+Bill after discount :: 333.33
+Bill after discount :: 81493.08
+*/
