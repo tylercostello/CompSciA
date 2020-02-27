e095dd2a0508c3e640ef090371f3b2cb9a5543b6
tyler costello <34726618+tylercostello@users.noreply.github.com>
2020-02-27 09:46:42 -0800

    a

---
+++ APCSA/src/Elevens/Card.java
@@ -0,0 +1,27 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+package Elevens;
+public class Card
+{
+	public static final String FACES[] = {"ZERO","ACE","TWO","THREE","FOUR",
+			"FIVE","SIX","SEVEN","EIGHT","NINE","TEN","JACK","QUEEN","KING"};
+
+	//instance variables
+		//String suit
+		//int face
+
+  	//constructors
+
+
+	// modifiers
+		//set methods
+
+
+  	//accessors
+		//get methods
+
+
+  	//toString
+
+ }
