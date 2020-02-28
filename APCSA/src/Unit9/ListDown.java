abe6811439f3f94ce54be000a1e56ff7535688a5
tyler costello <34726618+tylercostello@users.noreply.github.com>
2020-02-28 10:48:05 -0800

    Unit 9 with test cases

--- APCSA/src/Unit9/ListDown.java
+++ APCSA/src/Unit9/ListDown.java
@@ -12,6 +12,9 @@ public class ListDown
 	//are in decreasing order [31,12,6,2,1]
 	public static boolean go(List<Integer> numArray)
 	{
+		if (numArray.size()==1) {
+			return false;
+		}
 		boolean dec = true;
 		for (int i=1;i<numArray.size();i++) {
 			if (numArray.get(i)>=numArray.get(i-1)) {
