bbc7d02ed1b4a28bd532036799e18a7c4b3a5d8b
tyco333 <34726618+tyco333@users.noreply.github.com>
2020-03-02 21:37:17 -0800

    finished unit11

--- APCSA/src/Unit11/Doggies.java
+++ APCSA/src/Unit11/Doggies.java
@@ -11,24 +11,44 @@ public class Doggies
 
 	public Doggies(int size)
 	{
-		//point pups at a new arry of Dog
+		//point pups at a new array of Dog
+		pups=new Dog[size];
 		
 	}
 	
 	public void set(int spot, int age, String name)
 	{
+		if (spot<pups.length){
+		pups[spot]=new Dog(age,name);
+		}
 		//put a new Dog in the array at spot 
 		//make sure spot is in bounds		
 	}
 
 	public String getNameOfOldest()
 	{
-		return null;
+		String oldestName="";
+		int oldestAge=0;
+		for (Dog dog : pups){
+			if (dog.getAge()>oldestAge){
+				oldestName=dog.getName();
+				oldestAge=dog.getAge();
+			}
+		}
+		return oldestName;
 	}
 
 	public String getNameOfYoungest()
 	{
-		return null;
+		String youngestName="";
+		int youngestAge=1000;
+		for (Dog dog : pups){
+			if (dog.getAge()<youngestAge){
+				youngestName=dog.getName();
+				youngestAge=dog.getAge();
+			}
+		}
+		return youngestName;
 	}
 
 	public String toString()
