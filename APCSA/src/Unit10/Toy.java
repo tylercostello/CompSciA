3d5a06eef46c801780439fcf5937abcd1407f484
tyler costello <34726618+tylercostello@users.noreply.github.com>
2020-03-05 11:12:37 -0800

    deck and unit 10

---
+++ APCSA/src/Unit10/Toy.java
@@ -0,0 +1,46 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+package Unit10;
+import static java.lang.System.*;
+
+public class Toy
+{
+	private String name;
+	private int count;
+
+	public Toy()
+	{
+		
+	}
+
+	public Toy( String nm )
+	{
+		name=nm;
+	}
+	
+	public int getCount()
+	{
+		return count;
+	}
+	
+	public void setCount( int cnt )
+	{
+		count=cnt;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public void setName( String nm )
+	{
+		name=nm;
+	}
+
+	public String toString()
+	{
+	   return name+" "+count;
+	}
+}
---
+++ APCSA/src/Unit10/ToyRunner.java
@@ -0,0 +1,17 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+package Unit10;
+import static java.lang.System.*;
+
+public class ToyRunner
+{
+	public static void main(String[] args)
+	{
+		Toy mytoy=new Toy();
+		mytoy.setName("Bean");
+		mytoy.setCount(3);
+		System.out.println(mytoy.toString());
+		
+	}
+}
