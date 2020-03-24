d9137545319b115da42629a64aa307d4eac41ca6
tyco333 <34726618+tyco333@users.noreply.github.com>
2020-03-24 13:49:44 -0700

    Finished Number Sorted

--- APCSA/src/Unit13/NumberSorter.java
+++ APCSA/src/Unit13/NumberSorter.java
@@ -15,12 +15,41 @@ public class NumberSorter
 	private static int getNumDigits(int number)
 	{
 		int count = 0;
+		while (number>0){
+			count++;
+			number=number/10;
+		}
 		return count;
 	}
 
 	public static int[] getSortedDigitArray(int number)
 	{
-		int[] sorted = null;
+		
+		int digits=getNumDigits(number);
+		int tempNumber=number;
+		int freeSpot=0;
+		int[] sorted = new int[digits];
+		int[] tempArray = new int[digits];
+		for (int i=digits-1;i>=0;i--){
+			tempArray[i]=tempNumber%10;
+			tempNumber=tempNumber/10;
+		}
+		for (int i=0;i<digits;i++){
+			int min=tempArray[0];
+			int minSpot=0;
+			for (int j=0;j<digits;j++){
+				if (tempArray[j]<min){
+					min=tempArray[j];
+					minSpot=j;
+				}
+			}
+			tempArray[minSpot]=10;
+			sorted[freeSpot]=min;
+			freeSpot++;
+		}
+		
+		
+		
 		return sorted;
 	}
 }
--- APCSA/src/Unit13/NumberSorterRunner.java
+++ APCSA/src/Unit13/NumberSorterRunner.java
@@ -12,6 +12,7 @@ public class NumberSorterRunner
 		int[] cases = {567891, 901912468, 864213507, 898777, 234422};
 		for( int test : cases )
 		{
+			
 			int[] one = NumberSorter.getSortedDigitArray( test );
 			for(int item : one)
 			{
