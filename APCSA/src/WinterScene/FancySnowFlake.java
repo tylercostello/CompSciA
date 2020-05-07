17c40a7bfc597cf42e5e3f1118d1da1e235350e0
tyco333 <34726618+tyco333@users.noreply.github.com>
2020-05-06 23:31:15 -0700

    Created Winter Scene

---
+++ APCSA/src/WinterScene/FancySnowFlake.java
@@ -0,0 +1,11 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+package WinterScene;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Polygon;
+
+//extend the AbstractShape class to make a FancySnowFlake class
+
+
